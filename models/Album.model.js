5f3a7b38bcc5d375459810e4b358cd83c109a234
KimSteenBekke <Kim.Steen.Bekke@lego.com>
2022-02-08 10:59:57 +0100

    updating models

--- models/Album.model.js
+++ models/Album.model.js
@@ -1,18 +1,22 @@
 const { Schema, model } = require("mongoose");
 
-const albumSchema = new Schema(
+const track = new Schema({
+  trackSequence: Number,
+  trackTitle: String,
+  trackTime: Number,
+});
+
+const album = new Schema(
   {
     title: String,
     artist: String,
     label: String,
-    trackSequence: Number,
-    trackTitle: String,
-    trackTime: Number,
     genre: String,
+    tracks: [track],
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = model("Album", albumSchema);
+module.exports = model("Album", album);
