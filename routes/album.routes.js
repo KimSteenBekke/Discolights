9e694c20c4b5ada9b685ff547ea295aa72c60e1e
Peter Ravnsbaek <84021050+dkpetrav@users.noreply.github.com>
2022-02-08 14:28:52 +0100

    more field on Album (#6)
    Co-authored-by: Peter Ravnsbæk <dkpetrav@lego.com>

--- routes/album.routes.js
+++ routes/album.routes.js
@@ -8,8 +8,8 @@ const Album = require('../models/Album.model.js');
 router.get('/albums/create', (req, res) => res.render('album-views/album-create.hbs'));
 
 router.post('/albums/create', (req, res, next) => {
-    const { title, artist } = req.body;
-    Album.create({ title, artist })
+    const { title, artist, label, genre, tracks } = req.body;
+    Album.create({ title, artist, label, genre, tracks })
     .then(() => res.redirect('/albums/create'))
     .catch(error => next(error));
   });
