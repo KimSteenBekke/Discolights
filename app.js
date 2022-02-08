72ef874789a52374416a3ba68bc3c510c15e8355
Peter Ravnsbæk <dkpetrav@lego.com>
2022-02-08 13:28:04 +0100

    first save to DB

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 
+const albumRouter = require('./routes/album.routes'); 
+app.use('/', albumRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
