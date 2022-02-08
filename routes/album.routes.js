d2ec2ddf3fcdc53868c839f6f202d56277accf11
Peter Ravnsbaek <84021050+dkpetrav@users.noreply.github.com>
2022-02-08 13:30:42 +0100

    first save to DB (#5)
    Co-authored-by: Peter Ravnsbæk <dkpetrav@lego.com>

---
+++ routes/album.routes.js
@@ -0,0 +1,19 @@
+// routes/album.routes.js
+
+const express = require('express');
+const router = express.Router();
+const Album = require('../models/Album.model.js');
+
+// GET route to display the form to create a new album
+router.get('/albums/create', (req, res) => res.render('album-views/album-create.hbs'));
+
+router.post('/albums/create', (req, res, next) => {
+    const { title, artist } = req.body;
+    Album.create({ title, artist })
+    .then(() => res.redirect('/albums/create'))
+    .catch(error => next(error));
+  });
+   
+
+
+module.exports = router;
